380ecc49a14297db96f3315770bf7e172353637d
Souhayb120 <souhaybhadi06@gmail.com>
2026-08-13 12:30:53 +0100

    first commit

---
+++ src/main/java/com/example/kottabi/KottabiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kottabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KottabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KottabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kottabi/KottabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kottabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KottabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
