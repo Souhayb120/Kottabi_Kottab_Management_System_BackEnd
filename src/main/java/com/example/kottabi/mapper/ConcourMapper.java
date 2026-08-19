1d1348a8a99ef6b6c061aa1c33ff6766ba27d0f5
Souhayb120 <souhaybhadi06@gmail.com>
2026-08-19 01:00:15 +0100

    add Mappers

---
+++ src/main/java/com/example/kottabi/mapper/ConcourMapper.java
@@ -0,0 +1,6 @@
+package com.example.kottabi.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ConcourMapper {}
---
+++ src/main/java/com/example/kottabi/mapper/PresenceMapper.java
@@ -0,0 +1,7 @@
+package com.example.kottabi.mapper;
+
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PresenceMapper {}
